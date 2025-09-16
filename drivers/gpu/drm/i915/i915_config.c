f752a9e6ceca1c2f501fca5b9f5503b6e69bdc32
veelove010902 <veelove010902@2925.com>
2025-09-16 19:41:16 +0800

    sffppff

--- drivers/gpu/drm/i915/i915_config.c
+++ drivers/gpu/drm/i915/i915_config.c
@@ -11,8 +11,11 @@
 unsigned long
 i915_fence_context_timeout(const struct drm_i915_private *i915, u64 context)
 {
-	if (CONFIG_DRM_I915_FENCE_TIMEOUT && context)
+	/* Some downstream kernels may not define CONFIG_DRM_I915_FENCE_TIMEOUT. */
+	#if defined(CONFIG_DRM_I915_FENCE_TIMEOUT) && (CONFIG_DRM_I915_FENCE_TIMEOUT)
+	if (context)
 		return msecs_to_jiffies_timeout(CONFIG_DRM_I915_FENCE_TIMEOUT);
+	#endif
 
 	return 0;
 }
